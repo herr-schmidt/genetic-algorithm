11a6f828286980e37fbb1c58eda1652a2dbd1672
herr-schmidt <matte24694@gmail.com>
2024-10-26 22:44:47 +0200

    pass fitness function

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,27 @@ use crate::ga::{Domain, DomainCategory, GAOptimizer};
 
 mod ga;
 
+fn my_function() {
+    println!("HA!");
+}
+
+fn fitness(solution: &Vec<f64>) -> f64 {
+    println!("{:?}", solution);
+    my_function();
+    solution.iter().sum()
+}
+
 fn main() {
     let mut domains = vec![];
-    for _ in 0..500000 {
-        domains.push(Domain::new(DomainCategory::Integers, None));
+    for _ in 0..200 {
+        domains.push(Domain::new(DomainCategory::Reals, None));
     }
 
-    let mut ga_optimizer = GAOptimizer::new(500000, domains, 0, 40000);
+    let mut ga_optimizer = GAOptimizer::new(3, domains, 10, 5, fitness);
 
     // for domain in &ga_optimizer.gene_domains {
     //     println!("{}", domain)
     // }
 
-    ga_optimizer.initialize_population();
+    ga_optimizer.run();
 }
