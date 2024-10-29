625da63232d378aaa6edfd560b7f2a50689d7347
herr-schmidt <matte24694@gmail.com>
2024-10-29 21:37:01 +0100

    simple bounded knapsack problem

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,32 @@ fn my_function() {
 }
 
 fn fitness(solution: &Vec<f64>) -> f64 {
-    // println!("{:?}", solution);
-    my_function();
-    solution.iter().sum()
+    let mut solution_weight = 0.;
+    let mut solution_profit = 0.;
+    for (i, x) in solution.iter().enumerate() {
+        solution_weight = solution_weight + x * WEIGHTS[i];
+        solution_profit = solution_weight + x * PROFITS[i];
+    }
+
+    if solution_weight > WEIGHT_LIMIT {
+        return - solution_weight
+    }
+
+    solution_profit
 }
+
+static WEIGHTS: [f64; 10] = [4., 3., 7., 2., 10., 1., 12., 2., 9., 17.];
+static PROFITS: [f64; 10] = [12., 1., 6., 2., 7., 3., 5., 4., 8., 10.];
+static WEIGHT_LIMIT: f64 = 100.;
 
 fn main() {
     let mut domains = vec![];
-    let genes = 25;
+    let genes = 10;
     for _ in 0..genes {
-        domains.push(Domain::new(DomainCategory::Integers, Some(vec![-3., 0.])));
+        domains.push(Domain::new(DomainCategory::Integers, Some(vec![0., 10.])));
     }
 
-    let mut ga_optimizer = GAOptimizer::new(genes, domains, 10000, 500, 0.2, fitness);
+    let mut ga_optimizer = GAOptimizer::new(genes, domains, 500000, 10000, 0.4, fitness);
 
     // for domain in &ga_optimizer.gene_domains {
     //     println!("{}", domain)
